986598ba49e465113310b8e8629703e85448448c
ermiatha <ermioni.athanasiadi@student.uhasselt.be>
2026-05-18 11:20:38 +0200

    - add relevant datasets for analysis - update shiny app: add Steve's visualisations - add Steve's figures

--- app.R
+++ app.R
@@ -7,9 +7,11 @@ options(shiny.error = browser)
 # Install packages if needed:
 # install.packages(c("shiny", "dplyr", "ggplot2", "DT"))
 library(shiny)
-library(dplyr)
-library(ggplot2)
 library(DT)
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(fmsb)
 
 # Data ------------------------------------------------------------------------
 aqi_joined <- readRDS("./data/aqi_joined.RDS")
@@ -18,41 +20,119 @@ aqi_joined <- aqi_joined %>%
         pollutant = as.factor(pollutant)
     )
 
+madrid_complete <- readRDS("./data/madrid_complete.RDS")
+madrid_complete <- madrid_complete %>%
+    mutate(
+        year = as.factor(year)
+    )
 
+radar_prep_2008 <- madrid_complete %>%
+    filter(year == 2008) %>%
+    #filter(name %in% target_names) %>%
+    group_by(name) %>%
+    summarise(
+        NO_2 = mean(NO_2, na.rm = TRUE),
+        O_3  = mean(O_3, na.rm = TRUE),
+        PM10 = mean(PM10, na.rm = TRUE),
+        CO   = mean(CO, na.rm  = TRUE),
+        SO_2 = mean(SO_2, na.rm = TRUE),
+        .groups = "drop"
+    ) %>%
+    # Rescale pollutants 0-100 for visual comparison
+    mutate(across(-name, ~ scales::rescale(.x, to = c(0, 100))))
+
+stations_2008 <- radar_prep_2008 %>% select(name) %>% drop_na() %>%
+    pull(name) %>% unique()
+
+
+plt_lineplot_stations <- readRDS("./Steve/figures/lineplot_stations.rds")
 
 # User interface --------------------------------------------------------------
 ui <- fluidPage(
-    titlePanel("Air Pollution in Madrid"),
-
     
-    sidebarLayout(
-        sidebarPanel(
-            width = 2,
+    titlePanel("Air Pollution in Madrid"),
+    
+    br(),
+    
+    h3("Summary"),
+    verbatimTextOutput("summary_text"),
+    
+    br(),
+    
+    h3("Dataset"),
+    DTOutput("pollutant_table"),
+    
+    br(),
+    
+    # ---------------------------------------------------
+    # Visualisation 1: Heatmap
+    # ---------------------------------------------------
+    
+    h2("AQI Levels over Time"),
+    
+    fluidRow(
+        column(
+            width = 3,
             
             selectInput(
                 inputId = "pollutant",
                 label = "Choose Pollutant",
                 choices = c("All", levels(aqi_joined$pollutant)),
                 selected = "All"
-                )
-
-
-        ),
-        
-        mainPanel(
-            width = 10,
-            h3("Summary"),
-            verbatimTextOutput("summary_text"),
-            
-            h3("Dataset"),
-            DTOutput("pollutant_table"),
-            
-            h3("AQI Levels over Time"),
-            plotOutput("plt_heatmap_over_time", height = "600px")
+            )
         )
-        
-        
+    ),
+    
+    plotOutput(
+        "plt_heatmap_over_time",
+        height = "650px"
+    ),
+    
+    br(),
+    br(),
+    
+    # ---------------------------------------------------
+    # Visualisation 2: line plot
+    # ---------------------------------------------------
+    
+    
+    h2("Line Plot: Pollution Evolution over the Years"),
+    
+    plotOutput(
+        "plt_lineplot_stations",
+        height = "650px"
+    ),
+    
+    br(),
+    br(),
+    
+    # ---------------------------------------------------
+    # Visualisation 3: Radar plot
+    # ---------------------------------------------------
+    
+    h2("Radar Plot: Pollution across Stations"),
+    
+    fluidRow(
+        column(
+            width = 3,
+            
+            sliderInput(
+                inputId = "year",
+                label = "Choose Year",
+                min = min(as.numeric(as.character(madrid_complete$year)), na.rm = TRUE),
+                max = max(as.numeric(as.character(madrid_complete$year)), na.rm = TRUE),
+                value = 2008,
+                step = 1,
+                sep = ""
+            )
+        )
+    ),
+    
+    plotOutput(
+        "plt_radar_over_time",
+        height = "850px"
     )
+    
 )
 
 # Server ----------------------------------------------------------------------
@@ -155,6 +235,74 @@ server <- function(input, output) {
             )
         p        
         }, width = 1400, height = 400)
+    
+    
+    ### Line plot ############################################################
+    # no interactions
+    output$plt_lineplot_stations <- renderPlot({
+        plt_lineplot_stations
+    })
+    
+    ### Radar plot ############################################################
+    output$plt_radar_over_time <- renderPlot({
+
+        
+        ### receive user input
+        data <- madrid_complete
+        if (input$year != "All") {
+            data <- data %>% 
+                filter(year == input$year)
+        }
+        
+        radar_prep_2018 <- data %>%
+            filter(name %in% stations_2008) %>%
+            group_by(name) %>%
+            summarise(
+                NO_2 = mean(NO_2, na.rm = TRUE),
+                O_3  = mean(O_3, na.rm = TRUE),
+                PM10 = mean(PM10, na.rm = TRUE),
+                CO   = mean(CO, na.rm  = TRUE),
+                SO_2 = mean(SO_2, na.rm = TRUE),
+                .groups = "drop"
+            ) %>%
+            # Rescale pollutants 0-100 for visual comparison
+            mutate(across(-name, ~ scales::rescale(.x, to = c(0, 100))))
+        
+        radar_df <- as.data.frame(t(radar_prep_2018[,-1]))
+        colnames(radar_df) <- radar_prep_2018$name
+        
+        
+        radar_final <- rbind(rep(100, ncol(radar_df)), rep(0, ncol(radar_df)), radar_df)
+        
+        gas_colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00", "#A65628")
+        gas_fill   <- alpha(gas_colors, 0.2)
+        
+        radarchart(radar_final, 
+                   axistype = 1,
+                   pcol = gas_colors, 
+                   pfcol = gas_fill, 
+                   plwd = 3, 
+                   plty = 1,
+                   cglcol = "grey80", 
+                   cglty = 1, 
+                   axislabcol = "grey30", 
+                   caxislabels = seq(0, 100, 25), 
+                   cglwd = 0.8,
+                   vlcex = 0.8,
+                   title = "Pollutant Footprint: 6 Priority Gases Across Top Stations")
+        
+        legend(x = 1.1, y = 1.2,
+               #x = "topright", 
+               #inset = c(-0.15, 0), # Move slightly outside the plot area
+               legend = rownames(radar_df), 
+               bty = "n", 
+               pch = 20, 
+               col = gas_colors, 
+               cex = 0.85, 
+               pt.cex = 1.5,
+               text.font = 2)
+        
+    })
     
 }
 
